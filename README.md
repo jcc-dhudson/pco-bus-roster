36893608c960026a730610e87754dcb950427beb
jcc <jcc@med-nodered-10.jcc-media.local>
2022-06-25 20:01:35 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PCO Bus Roster
+Slapping something together to possibly check kids into a bus by pulling registrations from Planning Center Online.
---
+++ bus-roster-ws.py
@@ -0,0 +1,22 @@
+import sys
+import os
+import pypco
+import shelve
+from flask import Flask, jsonify, render_template, request, send_from_directory
+
+
+def logger(msg):
+    time = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+    print(f"{time} - {msg}")
+
+
+try:
+    PCO_APP_ID = os.environ['PCO_APP_ID']
+    PCO_SECRET = os.environ['PCO_SECRET'] # source ~/pco-env-vars.sh
+except Exception as e:
+    print(f"Must supply PCO_APP_ID, PCO_SECRET as environment vairables. - {e}")
+    sys.exit(1)
+
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='static',)
